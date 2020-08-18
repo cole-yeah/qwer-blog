58b65b6e75378d30fdaa6744adcaad25a7d79f20
cole-yeah <243461254@qq.com>
2020-08-18 22:58:40 +0800

    update project

--- config/index.js
+++
@@ -1,9 +0,0 @@
-const opts = {
-  folder: '/Users/cole/works/projects/h5-mall-new/src',
-  datas: [{
-    file: 'test',
-    type: 'page',
-  }]
-}
-
-module.exports = opts
--- src/index.js
+++ src/index.js
@@ -1,44 +1,17 @@
-const fs = require('fs')
-const path = require('path')
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-const scssTpl = require('../template/scss')
-const pageTpl = require('../template/page')
-const config = require('../config/index')
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-const { datas, folder } = config
-
-const mkdirFunc = async (dirname, a) => {
-  
-  if(fs.existsSync(dirname)) {
-    return true
-  }
-  
-  const res = await fs.mkdirSync(dirname).catch(err => err)
-  console.log('-- success --', res)
-}
-
-const handleAction = (err) => {
-  if(err) {
-    console.log('xx fail xx', err)
-    return true
-  }
-  console.log('-- success --')
-}
-
-const touchFile = ({
-  file,
-  type
-}) => {
-  if(type === 'page') {
-    fs.writeFile(`${folder}/${file}/index.scss`, scssTpl(), handleAction)
-    fs.writeFile(`${folder}/${file}/index.js`, pageTpl(), handleAction)
-  }
-}
-
-datas.map((data) => {
-  const { file } = data
-  const completeUrl = `${folder}/${file}`
-  if(mkdirFunc(completeUrl)) {
-    touchFile(data)
-  }
-})
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
